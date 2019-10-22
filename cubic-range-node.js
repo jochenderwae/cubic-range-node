ca188a1018297e757acd01cb43138e34fc79bf42
jochenderwae <jochen.derwae@gmail.com>
2019-10-22 20:59:09 +0200

    Added package-lock.json Added .gitignore

--- cubic-range-node.js
+++ cubic-range-node.js
@@ -14,7 +14,9 @@ module.exports = function(RED) {
         var node = this;
 		
         node.on('input', function(msg) {
+			// only allow changes to the table if node.acceptNewTable is set to true
 			if(node.acceptNewTable && msg.table) {
+				// check if the input is sensible
 				if(Array.isArray(msg.table.x) && 
 					Array.isArray(msg.table.y) && 
 					msg.table.x.length == msg.table.y.length && 
@@ -26,15 +28,18 @@ module.exports = function(RED) {
 				}
 			}
 			
+			// skip this message if it's the topic to change the table
+			if(node.acceptNewTable && msg.topic === node.newTableTopic) {
+				node.log("Skipping message with topic: " + node.newTableTopic);
+				return;
+			}
+			
+			// get the property, put it through the spline and write the value out to the property again
 			var value = RED.util.getMessageProperty(msg, node.property);
 			if(value !== undefined) {
 				var n = Number(value);
 				if (!isNaN(n)) {
-					if(!node.acceptNewTable || msg.topic !== node.newTableTopic) {
-						value = node.spline.at(n);
-						/*node.log(n + ": " + value);*/
-					}
-
+					value = node.spline.at(n);
 					RED.util.setMessageProperty(msg, node.property, value);
 					node.send(msg);
 				} else {
